89972b37657d080fbbae51b1575c04e6f2e99882
ileeds <ileeds@brandeis.edu>
2017-10-14 19:08:04 -0400

    new user email downcase added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ApplicationRecord
   has_many :products
   validates :name, presence: true
   validates :email, presence: true
+  before_save   :downcase_email
   has_secure_password
-  
+
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
 end
