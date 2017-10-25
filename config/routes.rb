c31e9cbb04ef1fdbba8508a6058fd26c2138c255
jtx365 <turnerj@brandeis.edu>
2017-10-25 19:31:51 -0400

    basic working searches

--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,5 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  post '/searches', to: 'seaches#create'
 end
