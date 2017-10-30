1c2e15407d53000a05d652dc012676e4467cc117
jtx365 <turnerj@brandeis.edu>
2017-10-30 11:09:38 -0400

    fixed flashing of not brandeis google email, temporary fix for not allowing user to go to another user's page, change buttons for signing in with brandeis user account

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,6 +4,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:one)
+    @user = users(:two)
   end
 
   test "login with invalid information" do
@@ -35,4 +36,5 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
 end
