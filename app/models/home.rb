d316fca5cde57d34d92f3dc7ef3d13da33830652
ileeds <ileeds@brandeis.edu>
2017-11-18 20:09:15 -0500

    improved date picker

--- app/models/home.rb
+++ app/models/home.rb
@@ -122,8 +122,8 @@ class Home < ApplicationRecord
 
   # date range of availability, can choose both start and end
   scope :with_availability_range, lambda { |date_range_attrs|
-    start_date = Date.strptime(date_range_attrs.start_date, '%m/%d/%Y') rescue nil
-    end_date = Date.strptime(date_range_attrs.end_date, '%m/%d/%Y') rescue nil
+    start_date = Date.strptime(date_range_attrs.dates.split(' - ')[0], '%m/%d/%Y') rescue nil
+    end_date = Date.strptime(date_range_attrs.dates.split(' - ')[1], '%m/%d/%Y') rescue nil
 
     if !start_date && !end_date
       return all
