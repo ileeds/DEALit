cdd73c95b343959a9519a96ad07d389ddb86f01e
Yan <yanshney@bramdeis.edu>
2017-11-24 17:21:04 -0500

    pictures and fixes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,6 +4,5 @@ class Photo < ApplicationRecord
   :path => ":rails_root/public/images/:id/:filename",
     :url  => "/images/:id/:filename",
     :styles => { :medium => "160x160!" }
-  attr_accessible :photo_file_name
   do_not_validate_attachment_file_type :photo
 end
